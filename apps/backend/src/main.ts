ada8e2f47d0b274bcd59f2cbc69192d205f1f3fb
Illamapalooza <juliusbaliling25@gmail.com>
2024-05-30 11:01:47 +0800

    Connect to supabase database

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,8 @@ import router from './router/app.router';
 import { uploadFilesRoute } from './app/uploads/uploads.contoller';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import pool from './database';
+
 import { aiNoteRoute } from './app/aiNote/aiNote.controller';
 
 dotenv.config();
@@ -11,21 +13,37 @@ dotenv.config();
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-
 const app = express();
 
-// Middleware
-app.use(router);
-app.use(express.json());
-app.use(cors());
+const connectToDatabase = async () => {
+  try {
+    await pool.connect();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database', error);
+  }
+};
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+const startServer = async () => {
+  // Middleware
+  app.use(router);
+  app.use(express.json());
+  app.use(cors());
 
-// Routes
+  // Routes
 
-app.use('/api', uploadFilesRoute, aiNoteRoute);
+  app.use('/api', uploadFilesRoute, aiNoteRoute);
+
+  app.use('/api/get-users', async (req, res) => {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  });
+
+  app.listen(port, host, () => {
+    console.log(`[ ready ] http://${host}:${port}`);
+  });
+};
+
+connectToDatabase().then(startServer);
 
 export default app;
